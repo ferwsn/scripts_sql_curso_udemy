40fb9dc39e9215b9717be7c76594ec29714b91a2
Fernando Walter <fernandowalter04@outlook.com>
2025-05-28 00:37:18 -0300

    Update indicador_clientes_meta.sql

--- indicador_clientes_meta.sql
+++ indicador_clientes_meta.sql
@@ -1,32 +1,32 @@
 	-- SIMULACAO INDICADOR DE CLIENTES POR META --
 	-- CONSIDERANDO META DE 5000 --
 			SELECT a.NomeCompleto,
-				   a.Contato,
-				   CONCAT(a.MesPeriodo,'/',a.AnoPeriodo) AS Periodo,
-				   SUM(a.Quantidade) AS Quantidade,
-				   SUM(a.Preco) AS Preco,
-				   (SUM(a.Quantidade) * SUM(a.Preco)) AS TotalMensal,
-				   CASE
-				        WHEN (SUM(a.Quantidade) * SUM(a.Preco)) >= 5000
-						THEN 'ATINGIDO'
-						ELSE 'N√O-ATINGIDO'
-				   END AS StatusMeta
+			       a.Contato,
+			       CONCAT(a.MesPeriodo,'/',a.AnoPeriodo) AS Periodo,
+			       SUM(a.Quantidade) AS Quantidade,
+			       SUM(a.Preco) AS Preco,
+			       (SUM(a.Quantidade) * SUM(a.Preco)) AS TotalMensal,
+			       CASE
+			            WHEN (SUM(a.Quantidade) * SUM(a.Preco)) >= 5000
+				    THEN 'ATINGIDO'
+				    ELSE 'N√ÉO-ATINGIDO'
+			       END AS StatusMeta
 			  FROM (SELECT (SELECT aa.NomeCompleto
-							  FROM tb_cliente aa
-							 WHERE aa.ClienteId = ped.ClienteId) AS NomeCompleto,
-						   (SELECT bb.Contato
-							  FROM tb_cliente bb
-							 WHERE bb.ClienteId = ped.ClienteId) AS Contato,
-						   MONTH(ped.DataPedido) AS MesPeriodo,
-						   YEAR(ped.DataPedido) AS AnoPeriodo,
-						   det.Quantidade,
-						   det.Preco
-					  FROM tb_pedido ped,
-						   tb_detalhe_pedido det
-					 WHERE ped.NumeroPedido = det.NumeroPedido) a
-          GROUP BY a.NomeCompleto,
-				   a.Contato,
-				   a.MesPeriodo,
-				   a.AnoPeriodo
-		  ORDER BY a.AnoPeriodo,
-		           a.MesPeriodo;		
+					  FROM tb_cliente aa
+					 WHERE aa.ClienteId = ped.ClienteId) AS NomeCompleto,
+				       (SELECT bb.Contato
+				          FROM tb_cliente bb
+				         WHERE bb.ClienteId = ped.ClienteId) AS Contato,
+				       MONTH(ped.DataPedido) AS MesPeriodo,
+			       	       YEAR(ped.DataPedido) AS AnoPeriodo,
+			    	       det.Quantidade,
+				       det.Preco
+				  FROM tb_pedido ped,
+				       tb_detalhe_pedido det
+				 WHERE ped.NumeroPedido = det.NumeroPedido) a
+                      GROUP BY a.NomeCompleto,
+			       a.Contato,
+			       a.MesPeriodo,
+		               a.AnoPeriodo
+		      ORDER BY a.AnoPeriodo,
+		               a.MesPeriodo;		
