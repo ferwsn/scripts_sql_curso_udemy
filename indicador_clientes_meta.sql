27320a1d97e5e9875ed19ecdd8e72c3af192e913
Fernando Walter <fernandowalter04@outlook.com>
2025-08-10 20:04:52 -0300

    Update indicador_clientes_meta.sql

--- indicador_clientes_meta.sql
+++ indicador_clientes_meta.sql
@@ -8,25 +8,27 @@
 			       (SUM(a.Quantidade) * SUM(a.Preco)) AS TotalMensal,
 			       CASE
 			            WHEN (SUM(a.Quantidade) * SUM(a.Preco)) >= 5000
-				    THEN 'ATINGIDO'
-				    ELSE 'NÃO-ATINGIDO'
+				        THEN 'ATINGIDO'
+				        ELSE 'NÃO-ATINGIDO'
 			       END AS StatusMeta
 			  FROM (SELECT (SELECT aa.NomeCompleto
-					  FROM tb_cliente aa
-					 WHERE aa.ClienteId = ped.ClienteId) AS NomeCompleto,
-				       (SELECT bb.Contato
-				          FROM tb_cliente bb
-				         WHERE bb.ClienteId = ped.ClienteId) AS Contato,
-				       MONTH(ped.DataPedido) AS MesPeriodo,
+					          FROM tb_cliente aa
+					         WHERE aa.ClienteId = ped.ClienteId) AS NomeCompleto,
+				           (SELECT bb.Contato
+				              FROM tb_cliente bb
+				             WHERE bb.ClienteId = ped.ClienteId) AS Contato,
+				           MONTH(ped.DataPedido) AS MesPeriodo,
 			       	       YEAR(ped.DataPedido) AS AnoPeriodo,
 			    	       det.Quantidade,
-				       det.Preco
-				  FROM tb_pedido ped,
-				       tb_detalhe_pedido det
-				 WHERE ped.NumeroPedido = det.NumeroPedido) a
-                      GROUP BY a.NomeCompleto,
+				           det.Preco
+				      FROM tb_pedido ped,
+				           tb_detalhe_pedido det
+				     WHERE ped.NumeroPedido = det.NumeroPedido) a
+          GROUP BY a.NomeCompleto,
 			       a.Contato,
 			       a.MesPeriodo,
-		               a.AnoPeriodo
-		      ORDER BY a.AnoPeriodo,
-		               a.MesPeriodo;		
+		           a.AnoPeriodo
+		  ORDER BY a.AnoPeriodo,
+		           a.MesPeriodo;
+                   GO
+
