6c38f1dde3436e252ea5372bd0a1e823c226c81a
Fernando Walter <fernandowalter04@outlook.com>
2025-08-10 20:01:07 -0300

    Update faturamento_categoria_ano.sql

--- faturamento_categoria_ano.sql
+++ faturamento_categoria_ano.sql
@@ -4,28 +4,26 @@
 	       SUM(a.Faturamento) AS FaturamentoAnual,
 	       ROUND(((SUM(CAST(a.Faturamento AS FLOAT)) / CAST(b.FaturamentoTotal AS FLOAT)) * 100),2) AS Percentual
 	  FROM (SELECT det.ProdutoId,
-		       prd.CategoriaId,
-		       cat.Descricao,
-		       det.Preco,
-		       det.Quantidade,
+		           prd.CategoriaId,
+		           cat.Descricao,
+		           det.Preco,
+		           det.Quantidade,
 	     	       ROUND((det.Preco * det.Quantidade),2) AS Faturamento,
-	   	       (SELECT YEAR(aa.DataPedido)
-			  FROM tb_pedido aa
-		         WHERE aa.NumeroPedido = det.NumeroPedido) AS DataPedido
-		  FROM tb_detalhe_pedido det,
-		       tb_produto prd,
-		       tb_categoria cat
-		 WHERE det.ProdutoId = prd.ProdutoId
-		   AND prd.CategoriaId = cat.CategoriaId) a,
+	   	           (SELECT YEAR(aa.DataPedido)
+			          FROM tb_pedido aa
+		             WHERE aa.NumeroPedido = det.NumeroPedido) AS DataPedido
+		      FROM tb_detalhe_pedido det,
+		           tb_produto prd,
+		           tb_categoria cat
+		     WHERE det.ProdutoId = prd.ProdutoId
+		       AND prd.CategoriaId = cat.CategoriaId) a,
 	       (SELECT ROUND(SUM(det.Preco * det.Quantidade),2) AS FaturamentoTotal
-		  FROM tb_detalhe_pedido det,
-		       tb_pedido ped
+		      FROM tb_detalhe_pedido det,
+		           tb_pedido ped
 	         WHERE det.NumeroPedido = ped.NumeroPedido
-		   AND ped.DataPedido LIKE '1996%') b
-	WHERE a.DataPedido = '1996'
-     GROUP BY a.Descricao,
-	      a.DataPedido,
-              b.FaturamentoTotal;
-     GROUP BY a.Descricao,
-	      a.DataPedido,
-              b.FaturamentoTotal;
+		       AND ped.DataPedido LIKE '1996%') b
+	  WHERE a.DataPedido = '1996'
+   GROUP BY a.Descricao,
+	        a.DataPedido,
+            b.FaturamentoTotal;
+
